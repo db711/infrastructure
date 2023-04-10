b2b04b293f03dbf32116f0b5b3c8724ae2a03705
Daniel Berger <Daniel.Berger@hs-rm.de>
2023-04-10 22:06:37 +0200

    Slight refactoring

--- compact_representations.c
+++ compact_representations.c
@@ -7,7 +7,7 @@ mulqi (GEN O, GEN a, GEN b, GEN a_, GEN b_)
     GEN res = cgetg(3,t_VEC);
     av = avma; gel(res,1) = gerepileupto(av,gdiv(gadd(gmul(a,a_),gmul(gmul(b,b_),gel(O,1))),gel(O,3)));
     av = avma; gel(res,2) = gerepileupto(av,gdiv(gadd(gmul(a,b_),gmul(b,a_)),gel(O,3)));
-    return gerepileupto(ltop, res);
+    return gerepileupto(ltop,res);
 }
 
 GEN expandcr(GEN O, GEN cr)
@@ -23,10 +23,7 @@ GEN expandcr(GEN O, GEN cr)
         fac = cgetg(3,t_VEC);
         av = avma; gel(fac,1) = gerepileupto(av,gdiv(gmael3(cr,i,1,1),gmael(cr,i+1,2)));
         av = avma; gel(fac,2) = gerepileupto(av,gdiv(gmael3(cr,i,1,2),gmael(cr,i+1,2)));
-        for (j = 1; j < l-i; j++)
-        {
-            fac = gerepileupto(av2,mulqi(O,gel(fac,1),gel(fac,2),gel(fac,1),gel(fac,2)));
-        }
+        for (j = 1; j < l-i; j++) fac = gerepileupto(av2,mulqi(O,gel(fac,1),gel(fac,2),gel(fac,1),gel(fac,2)));
         theta = gerepileupto(ltop,mulqi(O,gel(theta,1),gel(theta,2),gel(fac,1),gel(fac,2)));
     }
     return gerepileupto(ltop,theta);
