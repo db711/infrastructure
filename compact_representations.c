8b598b9b7a1cdbfbb025e40395ce0ee66eeb3f38
Daniel Berger <Daniel.Berger@hs-rm.de>
2023-04-16 20:48:38 +0200

    Implemented the missing case(s) of crmodm

--- compact_representations.c
+++ compact_representations.c
@@ -26,23 +26,58 @@ crmodm (GEN O, GEN cr, GEN m)
     pari_sp ltop = avma, av;
     GEN D, G, res, tmp;
     ulong l = lg(cr), i; 
-    if (!cmpii(gel(O,3),gen_2)) pari_err_IMPL("crmodm");
+    if (!cmpii(gel(O,3),gen_2) && !Mod2(m)) m = mulii(gen_2,m); //this case requires extra care
     av = avma;
     D = mkintmod(modii(gmael(cr,2,2),m),m);
     G = cgetg(3,t_VEC);
-    gel(G,1) = mkintmod(modii(gmael3(cr,1,1,1),m),m);
-    gel(G,2) = mkintmod(modii(gmael3(cr,1,1,2),m),m);
+    if (Mod2(m))
+    {
+        gel(G,1) = mkintmod(modii(gmael3(cr,1,1,1),m),m);
+        gel(G,2) = mkintmod(modii(gmael3(cr,1,1,2),m),m);
+    }
+    else
+    {
+        gel(G,1) = modii(gmael3(cr,1,1,1),m);
+        gel(G,2) = modii(gmael3(cr,1,1,2),m);
+    }
     for (i = 2; i < l-2; i++)
     {
         D = gmul(gmael(cr,i+1,2),gsqr(D));
-        G = mulqig(O,gmael(cr,i,1),mulqig(O,G,G));
+        if (Mod2(m)) G = mulqig(O,gmael(cr,i,1),mulqig(O,G,G));
+        else
+        {
+            tmp = mulqig(O,G,G);
+            gel(tmp,1) = modii(gel(tmp,1),m);
+            gel(tmp,2) = modii(gel(tmp,2),m);
+            tmp = mulqig(O,tmp,G);
+            G = cgetg(3,t_VEC);
+            gel(G,1) = modii(gel(tmp,1),m);
+            gel(G,2) = modii(gel(tmp,2),m);
+        }
         gerepileall(av,2,&D,&G);
     }
     D = gsqr(D);
-    G = mulqig(O,G,G);
+    if (Mod2(m)) G = mulqig(O,G,G);
+    else
+    {
+        tmp = mulqig(O,G,G);
+        G = cgetg(3,t_VEC);
+        gel(G,1) = modii(gel(tmp,1),m);
+        gel(G,2) = modii(gel(tmp,2),m);
+    }
     tmp = mulqig(O,G,mulqidivn(O,gmael(cr,l-1,1),gmael(cr,l-2,1),gmael(cr,l-1,2)));
-    res = cgetg(3,t_VEC);
-    gel(res,1) = gdiv(gel(tmp,1),D);
-    gel(res,2) = gdiv(gel(tmp,2),D);
+    if (Mod2(m)) 
+    {
+        res = cgetg(3,t_VEC);
+        gel(res,1) = gdiv(gel(tmp,1),D);
+        gel(res,2) = gdiv(gel(tmp,2),D);
+    }
+    else
+    {
+        m = diviiexact(m,gen_2);
+        res = cgetg(3,t_VEC);
+        av = avma; gel(res,1) = gerepileupto(av,modii(mulii(gel(tmp,1),ginvmod(lift(D),m)),m));
+        av = avma; gel(res,2) = gerepileupto(av,modii(mulii(gel(tmp,2),ginvmod(lift(D),m)),m));
+    }
     return gerepileupto(ltop,res);
 }
