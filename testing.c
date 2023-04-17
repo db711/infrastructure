3c07d73cf00eb8d6d489aee017c69897771ced74
Daniel Berger <Daniel.Berger@hs-rm.de>
2023-04-17 21:55:28 +0200

    Added testing function for computing smooth parts

--- testing.c
+++ testing.c
@@ -4,6 +4,7 @@
 
 int main() {
    pari_init(1000000000,0);
+   pari_close();
    return 0;
 }
 
@@ -13,7 +14,7 @@ timedtest(GEN (*f)(GEN O, ulong prec, long flag), ulong i, ulong n)
    pari_timer timer;
    pari_sp ltop = avma, av;
    GEN x, tmp; 
-   ulong j, t;
+   ulong j;
    tmp = powis(stoi(10),i);
    timer_start(&timer);
    av = avma;
@@ -24,13 +25,11 @@ timedtest(GEN (*f)(GEN O, ulong prec, long flag), ulong i, ulong n)
       f(rqoinit(x),DEFAULTPREC,0);
       set_avma(av);
    }
-   t = timer_delay(&timer);
-   set_avma(ltop);
-   return t;
+   return gc_long(ltop,timer_delay(&timer));
 }
 
 ulong 
-testcr(ulong i, ulong n)
+testcr(ulong i, ulong n, GEN m)
 {
    pari_sp ltop = avma, av, av2;
    GEN O, tmp, x, A, B, C, C_, y;
@@ -48,7 +47,7 @@ testcr(ulong i, ulong n)
       if (cmpri(gel(A,1),stoi(10)) < 0) continue; // in this case cr might fail
       av = avma; y = gerepileupto(av,roundr(gdiv(gel(A,1),mplog2(DEFAULTPREC))));
       size2 = avma;
-      B = cr(O,pci(O),y,ghalf,gen_0);
+      B = cr(O,pci(O),y,ghalf,m);
       size2 -= avma;
       if (size2 < size) count++;
       av = avma; 
@@ -61,9 +60,39 @@ testcr(ulong i, ulong n)
          C = C_; C_ = NULL;
       }
       if (abscmpii(gel(C,1),gmael(A,2,1)) || abscmpii(gel(C,2),gmael(A,2,2))) pari_printf("Error found for d = %Ps\n%Ps\nvs\n%Ps\n\n",x,C,gel(A,2));
-      else
       set_avma(av2);
    }
    set_avma(ltop);
    return count;
+}
+
+ulong 
+testcrsmoothpart(ulong i, ulong n, ulong B, ulong c, ulong sw)
+{
+   pari_timer timer;
+   pari_sp ltop = avma, av, av2;
+   GEN O, R, x, tmp, y, prod = gen_1, b;
+   ulong j, p = 2;
+   forprime_t S;
+   u_forprime_init(&S,2,B);
+   while ((p = u_forprime_next(&S))) prod = gerepileupto(ltop,mulis(prod,p));
+   tmp = powis(stoi(10),i);
+   timer_start(&timer);
+   av2 = avma;
+   for (j = 0; j < n; j++)
+   {
+      av = avma; x = gerepileupto(av,addii(randomi(tmp),tmp));
+      if (Z_issquare(x)) continue;
+      O = rqoinit(x);
+      b = pci(O);
+      if (Mod4(x) != 1) x = mulsi(4,x);
+      if (i >= 10) R = gel(quadclassunit0(x,0,NULL,DEFAULTPREC),4);
+      else R = quadregulator(x,DEFAULTPREC);
+      if (cmpri(R,stoi(10)) < 0) continue; // in this case cr might fail
+      av = avma; y = gerepileupto(av,roundr(gdiv(R,mplog2(DEFAULTPREC))));
+      if (sw == 1) crsmoothpart(O,gel(cr(O,b,y,ghalf,prod),2),B,c);
+      else crsmoothpart_alt(O,b,y,ghalf,B,c);
+      set_avma(av2);
+   }
+   return gc_long(ltop,timer_delay(&timer));
 }
--- testing.h
+++ testing.h
@@ -14,12 +14,25 @@ ulong timedtest(GEN (*f)(GEN O, ulong prec, long flag), ulong i, ulong n);
  * Assumes that the RNG of PARI is already set.
 */
 
-ulong testcr(ulong i, ulong n);
+ulong testcr(ulong i, ulong n, GEN m);
 /* Test compact representations.
- * Input:   Integers i and n.
+ * Input:   Integers i and n as well as m.
  * Output:  Number of times that the compact representation was smaller than the standard decimal representation.
  * The function tests n random squarefree d in the range [10^i, 10^(i+1))
    and outputs an error message if a compact representation is computed wrong.
+   The integer m is used to compute a representation where at most the factor that appears with to the power 1 shares any factors with m.
+ * Assumes that the RNG of PARI is already set.
+*/
+
+ulong testcrsmoothpart(ulong i, ulong n, ulong B, ulong c, ulong sw);
+/* Test compact representation smooth part.
+ * Input:   Integers i and n as well as B;
+            c = {1, 2};
+            sw = {1, 2}.
+ * Output:  The time it took in ms.
+ * The function tests either crsmoothpart or crsmoothpartalt (depending on wheter sw = 1 or sw = 2)
+   by computing the B-smooth part of the c'th coordinate of of a compact representation of the fundamental unit (x + y*sqrt(d))/s,
+   which is randomly chose in [10^i, 10^(i+1)); this is repeated n times.
  * Assumes that the RNG of PARI is already set.
 */
 
