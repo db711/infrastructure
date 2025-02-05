08c765b6923664d62c9f611bd3ed29110e0de073
Daniel Berger <13801737+db711@users.noreply.github.com>
2025-02-05 10:54:19 +0100

    changed t_VEC to t_VECSMALL where appropriate

--- stormer.c
+++ stormer.c
@@ -9,7 +9,7 @@ logsofprimes(ulong B, long prec)
 GEN
 createnode(GEN bv, GEN sol)
 {
-    if (typ(bv) != t_VEC) pari_err_TYPE("createnode",bv);
+    if (typ(bv) != t_VECSMALL) pari_err_TYPE("createnode",bv);
     if (typ(sol) != t_REAL) pari_err_TYPE("createnode",sol);
     GEN res = cgetg(3,t_VEC);
     gel(res,1) = shallowcopy(bv);
@@ -22,7 +22,7 @@ leftchild(GEN node, GEN lop)
 {
     if (lg(gel(node,1)) >= lg(lop)) return NULL; // node is a leaf
     GEN res = cgetg(3,t_VEC);
-    gel(res,1) = vec_append(gel(node,1),gen_0);
+    gel(res,1) = vecsmall_append(gel(node,1),0);
     gel(res,2) = gcopy(gel(node,2));
     return res;
 }
@@ -33,7 +33,7 @@ rightchild(GEN node, GEN lop)
     if (lg(gel(node,1)) >= lg(lop)) return NULL; // node is a leaf
     GEN res = cgetg(3,t_VEC);
     pari_sp ltop;
-    gel(res,1) = vec_append(gel(node,1),gen_1);
+    gel(res,1) = vecsmall_append(gel(node,1),1);
     ltop = avma; gel(res,2) = gerepileupto(ltop,addrr(gel(node,2),gel(lop,lg(gel(res,1))-1)));
     return res;
 }
@@ -63,7 +63,7 @@ stormer_print(ulong B, ulong top, long prec)
     av = avma; ub = gerepileupto(av,logr_abs(mulir(gen_2,addir(gen_1,gcosh(mulis(gen_2,top),prec))))); //can this be simplified?
     lop = logsofprimes(B,prec);
     av = avma;
-    vec = cgetg(1,t_VEC);
+    vec = cgetg(1,t_VECSMALL);
     node = gerepileupto(av,createnode(vec,itor(gen_0,prec)));
     pari_printf("%Ps\n",lop);
     pari_printf("upper bound: %Ps\n\n",ub);
