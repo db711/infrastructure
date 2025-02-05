51a99c01874a1fe2e1740d63637994309c8ba449
Daniel Berger <13801737+db711@users.noreply.github.com>
2025-02-05 06:39:06 +0100

    rewrite using PARI functions

--- stormer.c
+++ stormer.c
@@ -1,74 +1,38 @@
 #include "stormer.h"
 
 GEN
-logsofprimes(ulong B)
+logsofprimes(ulong B, ulong prec)
 {
-    return glog(primes((long)uprimepi(B)),DEFAULTPREC);
+    return glog(primes((long)uprimepi(B)),prec);
 }
 
-char*
-tlv2str(const tlv* vector, char* str)
+GEN
+createnode(GEN bv, GEN sol)
 {
-    ulong i;
-    i = *(vector->size);
-    while (i > 0)
-    {
-        i--;
-        if (vector->data[i] == zero) str[i] = '0';
-        else if (vector->data[i] == one) str[i] = '1';
-        else str[i] = 'x'; //fallthrough?
-    }
-    str[*(vector->size)] =  '\0';
-    return str;
+    //TODO: error handling
+    GEN res = cgetg(3,t_VEC);
+    gel(res,1) = shallowcopy(bv);
+    gel(res,2) = gcopy(sol);
+    return res;
 }
 
-tlv*
-str2tlv(const char* str, const ulong* size)
+GEN
+leftchild(GEN node)
 {
-    ulong i;
-    tl* data = malloc((*size)*sizeof(tl));
-    tlv* ret = malloc(sizeof(tlv));
-    i = (*size);
-    while (i > 0)
-    {
-        i--;
-        if (str[i] == '0') data[i] = zero;
-        else if (str[i] == '1')  data[i] = one;
-        else data[i] = undefined; //fallthrough?
-    }
-    ret->data = data;
-    ret->size = size;
-    return ret;
+    //TODO: error handling
+    GEN res = cgetg(3,t_VEC);
+    gel(res,1) = vec_append(gel(node,1),gen_0);
+    gel(res,2) = gcopy(gel(node,2));
+    return res;
 }
 
-node* 
-rightchild(const node* parent, node* child, GEN lop)
+GEN
+rightchild(GEN node, GEN lop)
 {
-    if (((parent->vector)->data)[0] != undefined) return NULL; //parent is a leaf
-    ulong i;
-    i = 1;
-    while (i < *(parent->vector->size) && (parent->vector->data)[i] == undefined)
-    {
-        (child->vector->data)[i-1] = undefined;
-        i++;
-    }
-    (child->vector->data)[i-1] = one;
-    for (;i < *(parent->vector->size); i++) (child->vector->data)[i] = (parent->vector->data)[i];
-    return child;
-}
-
-node* 
-leftchild(const node* parent, node* child, GEN lop)
-{
-    if (((parent->vector)->data)[0] != undefined) return NULL; //parent is a leaf
-    ulong i;
-    i = 1;
-    while (i < *(parent->vector->size) && (parent->vector->data)[i] == undefined)
-    {
-        (child->vector->data)[i-1] = undefined;
-        i++;
-    }
-    (child->vector->data)[i-1] = zero;
-    for (;i < *(parent->vector->size); i++) (child->vector->data)[i] = (parent->vector->data)[i];
-    return child;
+    //TODO: error handling
+    GEN res = cgetg(3,t_VEC);
+    pari_sp ltop;
+    gel(res,1) = vec_append(gel(node,1),gen_1);
+    ltop = avma; gel(res,2) = gerepileupto(ltop,addrr(gel(node,2),gel(lop,lg(gel(res,1))-1)));
+    return res;
 }
--- stormer.h
+++ stormer.h
@@ -1,60 +1,32 @@
 #ifndef STORMER_H
 #define STORMER_H
-#include <stdlib.h>
 #include <pari/pari.h>
 
-typedef enum
-{
-   zero,
-   one,
-   undefined
-} tl; //three-valued logic
-
-typedef struct 
-{
-   tl* data;
-   const ulong* size;
-} tlv; //three-valued logic vector
-
-typedef struct 
-{
-    tlv* vector;
-    GEN sol; //sum of logs
-} node;
-
-GEN logsofprimes(ulong B);
+GEN logsofprimes(ulong B, ulong prec);
 /* Logarithms of primes.
- * Input:   Smoothness bound B.
- * Output:  A GEN holding a t_VEC containing a sorted list of the logarithms of the primes <= B (which are themselves t_REAL of precision DEFAULTPREC).
+ * Input:   Smoothness bound B;
+            precision prec.
+ * Output:  log(primes(primepi(B))).
 */
 
-char* tlv2str(const tlv* vector, char* str);
-/* three-valued logic vector to string.
- * Input:   tlv vector; char* str.
-            User must ensure that str can store *(tlv.size)+1 many characters.
- * Ouput:   tlv.data converted to a word over the alphabet {0, 1, x}^* and stored in str.
+GEN createnode(GEN bv, GEN sol);
+/* Create node.
+ * Input:   bit vector bv \in {0, 1}^*;
+            real number sol.
+ * Output:  [bv, sol].
 */
 
-tlv* str2tlv(const char* str, const ulong* size);
-/* string to three-valued logic vector.
- * Input:   char* str, holding a word over the alphabet {0, 1, x}^*;
-            ulong* size, pointing to a global length parameter for tlvs.
-            User must ensure that the string is (at least) (*size) characters long.
- * Output:  A pointer to a tlv that is allocated in memory.
+GEN leftchild(GEN node);
+/* Left child.
+ * Input:   Node [bv, sol] (as returned by createnode or *child).
+ * Output:  Node [vec_append(bv,0), sol].
 */
 
-node* rightchild(const node* parent, node* child, GEN lop);
-/* right child.
- * Input:   node* parent, node* child;
-            GEN lop (logarithms of primes) as returned by logsofprimes (defined globally).
- * Output:  The right child of node parent is stored in child and returned.
-*/
-
-node* leftchild(const node* parent, node* child, GEN lop);
-/* left child.
- * Input:   node* parent, node* child;
-            GEN lop (logarithms of primes) as returned by logsofprimes (defined globally).
- * Output:  The left child of node parent is stored in child and returned.
+GEN rightchild(GEN node, GEN lop);
+/* Right child.
+ * Input:   Node [bv, sol] (as returned by createnode or *child);
+            lop (as returned by logsofprimes).
+ * Output:  Node [vec_append(bv,1), sol+lop(length(bv))].
 */
 
 #endif
