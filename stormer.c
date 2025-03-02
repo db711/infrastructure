8697f2fedc9bfd41785c6633dcdaa1cba12ccd73
Daniel Berger <13801737+db711@users.noreply.github.com>
2025-03-02 07:34:21 +0100

    rewrote stormer to use discriminants instead of their logs

--- stormer.c
+++ stormer.c
@@ -1,118 +1,80 @@
 #include "stormer.h"
 
-GEN
-logsofprimes(ulong B, long prec)
-{
-    return glog(primes((long)uprimepi(B)),prec);
-}
-
 static inline GEN
-createnode(GEN bv, GEN sol, GEN prev)
+createnode(GEN bv, GEN d, long size, GEN prev)
 /* Create node.
  * Input:   bit vector bv \in {0, 1}^*;
-            real number sol;
+            t_INT d;
+            size (in longwords) of the largest d to be stored in the tree;
             other node prev or NULL.
- * Output:  [bv, sol, prev].
-            (bv and sol are copied, prev is stored as a pointer only)
+ * Output:  [bv, d, prev].
+            (bv and d are copied, prev is stored as a pointer only)
 */
 {
     if (typ(bv) != t_VECSMALL) pari_err_TYPE("createnode",bv);
-    if (typ(sol) != t_REAL) pari_err_TYPE("createnode",sol);
+    if (typ(d) != t_INT) pari_err_TYPE("createnode",d);
     if (prev != NULL && typ(prev) != t_VEC) pari_err_TYPE("createnode",prev);
     GEN res = cgetg(4,t_VEC);
     gel(res,1) = gcopy(bv);
-    gel(res,2) = gcopy(sol);
+    gel(res,2) = cgeti(size); gaffect(d,gel(res,2));
     gel(res,3) = prev;
     return res;
 }
 
 static inline int
-isleaf (GEN node, GEN lop)
-/* is leaf?
- * Input:   node [bv, sol, prev] as created by createnode or leftchild/rightchild,
-            lop (as returned by logsofprimes).
- * Output:  1 if the node is a leaf, 0 otherwise.
-*/
-{
-    if (lg(gel(node,1)) >= lg(lop)) return 1;
-    return 0;
-}
+isleaf (GEN node, long length) { return (lg(gel(node,1)) > length); }
 
 static inline int
-isleftchild (GEN node)
-/* is left child?
- * Input:   node [bv, sol, prev] as created by createnode or leftchild/rightchild,
- * Output:  1 if the node is a left child, 0 otherwise.
-*/
-{
-    if (lg(gel(node,1)) > 1 && (long)gel(gel(node,1),lg(gel(node,1))-1) == 0) return 1;
-    return 0;
-}
+isleftchild (GEN node) { return (lg(gel(node,1)) > 1 && (long)gel(gel(node,1),lg(gel(node,1))-1) == 0); }
 
 static inline GEN
 leftchild(GEN node)
 /* Left child.
- * Input:   node [bv, sol, prev] (as returned by createnode or *child);
- * Output:  Node [vec_append(bv,0), sol, node].
+ * Input:   node [bv, d, prev] (as returned by createnode or *child);
+ * Output:  Node [vec_append(bv,0), d, node].
 */
 {
     GEN res = cgetg(4,t_VEC);
     gel(res,1) = vecsmall_append(gel(node,1),0);
-    gel(res,2) = gcopy(gel(node,2));
+    gel(res,2) = cgeti(gsizeword(gel(node,2))); gaffect(gel(node,2),gel(res,2));
     gel(res,3) = node;
     return res;
 }
 
 static inline GEN
-rightchild(GEN node, GEN lop)
+rightchild(GEN node, long length)
 /* Right child.
- * Input:   node [bv, sol, prev] (as returned by createnode or *child);
-            lop (as returned by logsofprimes);
- * Output:  Node [vec_append(bv,1), sol+lop(length(lop)-length(bv)), node];
+ * Input:   node [bv, d, prev] (as returned by createnode or *child);
+            length (of the considered list of primes).
+ * Output:  Node [vec_append(bv,1), d*prime(lg(bv)), node];
 */
 {
     GEN res = cgetg(4,t_VEC);
     pari_sp ltop;
-    if (lg(gel(node,1)) >= lg(lop)) pari_err_COMPONENT("rightchild",">=",gel(node,1),lop);
     gel(res,1) = vecsmall_append(gel(node,1),1);
-    ltop = avma; gel(res,2) = gerepileupto(ltop,addrr(gel(node,2),gel(lop,lg(lop)+1-lg(gel(res,1)))));
+    gel(res,2) = cgeti(gsizeword(gel(node,2)));
+    ltop = avma; gaffect(mulii(gel(node,2),prime(length-lg(gel(node,1))+1)),gel(res,2)); set_avma(ltop);
     gel(res,3) = node;
     return res;
 }
 
-static inline int
-checkrightchild(GEN node, GEN lop, GEN ub)
-/* Check right child.
- * Input:   node [bv, sol, prev] (as returned by createnode or *child);
-            lop (as returned by logsofprimes);
-            ub (upperbound) for sol in the nodes (may be t_INFINITY);
- * Output:  1 if the rightchild of node is within the ub, 0 otherwise.
-*/
-{
-    pari_sp ltop = avma;
-    int ret;
-    if (gcmp(addrr(gel(node,2),gel(lop,lg(lop)-lg(gel(node,1)))),ub) > 0) ret = 0;
-    else ret = 1;
-    set_avma(ltop); return ret;
-}
-
 GEN
-stormer_gen(GEN lop, GEN sol, GEN ub, GEN bv)
+stormer_gen(long length, GEN d, GEN ub, GEN bv)
 { 
-    if (typ(lop) != t_VEC) pari_err_TYPE("stormer_gen", lop);
-    if (typ(sol) != t_REAL) pari_err_TYPE("stormer_gen",sol);
+    if (typ(d) != t_INT) pari_err_TYPE("stormer_gen",d);
     if (bv != NULL && typ(bv) != t_VECSMALL) pari_err_TYPE("stormer_gen",bv);
     GEN node;
     pari_sp av = avma;
-    ulong i;
-    if (gcmp(sol,ub) > 0) return NULL;
-    node = gerepileupto(av,createnode(cgetg(1,t_VECSMALL),sol,NULL));
-    if (bv == NULL) while (!isleaf(node,lop)) node = leftchild(node);
+    long size, i;
+    if (cmpii(d,ub) > 0) return NULL;
+    size = gsizeword(ub);
+    node = gerepileupto(av,createnode(cgetg(1,t_VECSMALL),d,size,NULL));
+    if (bv == NULL) while (!isleaf(node,length)) node = leftchild(node);
     else
     {
         for (i = 1; i < lg(bv); i++)
         {
-            if ((long)gel(bv,i) == 1) node = rightchild(node,lop);
+            if ((long)gel(bv,i) == 1) node = rightchild(node,length);
             else node = leftchild(node);
         }
     }
@@ -120,22 +82,27 @@ stormer_gen(GEN lop, GEN sol, GEN ub, GEN bv)
 }
 
 GEN 
-stormer_next(GEN node, GEN lop, GEN ub)
+stormer_next(GEN node, long length, GEN ub)
 { 
-    GEN prev;
+    GEN prev, rc;
     pari_sp ltop = avma;
     do
     {
         prev = node;
         node = gel(node,3);
         if (node == NULL) return NULL;
-        if (isleftchild(prev) && checkrightchild(node,lop,ub))
+        if (isleftchild(prev))
         {
             set_avma((pari_sp)gel(node,2)); 
-            node = rightchild(node,lop);
-            while (!isleaf(node,lop)) node = leftchild(node);
-            set_avma(ltop);
-            return node;
+            rc = rightchild(node,length);
+            if (cmpii(ub,gel(rc,2)) < 0) set_avma(ltop);
+            else
+            {
+                node = rc;
+                while (!isleaf(node,length)) node = leftchild(node);
+                set_avma(ltop);
+                return node;
+            }
         }
     } while (1);
 }
--- stormer.h
+++ stormer.h
@@ -2,32 +2,25 @@
 #define STORMER_H
 #include <pari/pari.h>
 
-GEN logsofprimes(ulong B, long prec);
-/* Logarithms of primes.
- * Input:   Smoothness bound B;
-            precision prec.
- * Output:  log(primes(primepi(B))).
-*/
-
-GEN stormer_gen(GEN lop, GEN sol, GEN ub, GEN bv);
+GEN stormer_gen(long length, GEN d, GEN ub, GEN bv);
 /* Stormer generator. 
- * Input:   lop (as returned by logsofprimes);
-            sol (sum of logs) as a starting value in the nodes;
-            ub (upperbound) for sol in the nodes (may be t_INFINITY);
+ * Input:   length (of the considered list of primes);
+            d as a starting value in the nodes;
+            ub (upperbound) for d in the nodes;
             bv (bit vector) as a starting value, having the same length as lop (NULL is treated as [0, ..., 0]). 
- * Output:  A singly linked list starting at [bv, sol, prev]
+ * Output:  A singly linked list starting at [bv, d, prev] (if bv != NULL, this d is different than the input)
             with backlinks up to the root.
-            It is ensured that each sol stored in a node is not greater than ub.
+            It is ensured that each d stored in a node is not greater than ub.
             (bv has to have this property, this is not checked)
-            Returns NULL if sol > ub.
+            Returns NULL if d > ub.
 */
 
-GEN stormer_next(GEN node, GEN lop, GEN ub);
+GEN stormer_next(GEN node, long length, GEN ub);
 /* Stormer next.
  * Input:   node with (as returned by stormer_gen or stormer_next);
-            lop (as returned by logsofprimes);
-            ub (upperbound) for sol in the nodes (may be t_INFINITY);
- * Output:  A singly linked list [bv, sol, prev], starting at the next node, going back to root;
+            length (of the considered list of primes);
+            ub (upperbound) for sol in the nodes;
+ * Output:  A singly linked list [bv, d, prev], starting at the next node, going back to root;
             This function overwrites the part of the PARI stack taken up by the singly linked list returned by stormer_gen
             and restores avma before returning.
             Returns NULL if there are no more leaves to return.
